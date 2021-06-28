981fab30b2d2376a259276209e26a00eafb65918
Nithin Deepkumar <59648511+nndr229@users.noreply.github.com>
2021-06-28 14:29:10 +0530

    Update TodoList.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -165,13 +165,13 @@ function TodoList(props) {
 									className={classes.li}
 								>
 									<ListItemIcon>
-										<Checkbox
+// 										<Checkbox
 											// edge='start'
 // 											checked={checked.indexOf(idx) !== -1}
 // 											tabIndex={-1}
 											// disableRipple
-											inputProps={{ 'aria-labelledby': labelId }}
-										/>
+// 											inputProps={{ 'aria-labelledby': labelId }}
+// 										/>
 									</ListItemIcon>
 									<ListItemText
 										className={
